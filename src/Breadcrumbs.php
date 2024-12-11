340d212e576c4c210fd5085eb29d671212a4992b
Brian Faust <hello@basecode.sh>
2024-12-11 06:23:43 +0200

    first commit
    Signed-off-by: Brian Faust <hello@basecode.sh>

---
+++ src/Breadcrumbs.php
@@ -0,0 +1,62 @@
+<?php declare(strict_types=1);
+
+/**
+ * Copyright (C) BaseCode Oy - All Rights Reserved
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+namespace BaseCodeOy\Breadcrumbs;
+
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Routing\Router;
+
+final class Breadcrumbs
+{
+    /** @var array<string, callable> */
+    private array $callbacks = [];
+
+    public function __construct(
+        private readonly Generator $generator,
+        private readonly Router $router,
+        private readonly Factory $viewFactory,
+    ) {
+        //
+    }
+
+    public function for(string $name, callable $callback): void
+    {
+        $this->callbacks[$name] = $callback;
+    }
+
+    public function render(?string $name = null, array $parameters = [], string $view = 'full-width-bar'): string
+    {
+        return $this->renderWithView($name, $parameters, 'breadcrumbs::'.$view);
+    }
+
+    public function renderWithView(string $name, array $parameters, string $view): string
+    {
+        return $this->viewFactory->make($view, [
+            'breadcrumbs' => $this->generate($name, $parameters),
+        ])->render();
+    }
+
+    public function generate(?string $name = null, array $parameters = []): array
+    {
+        [$actualName, $actualParameters] = $this->getCurrentRoute($name, $parameters);
+
+        return $this->generator->generate($this->callbacks, $actualName, $actualParameters);
+    }
+
+    private function getCurrentRoute(?string $name = null, array $parameters = []): array
+    {
+        if ($name !== null && $name !== '' && $name !== '0' && $parameters !== []) {
+            return [$name, $parameters];
+        }
+
+        $route = $this->router->current();
+
+        return [$route->getName(), $route->parameters()];
+    }
+}
---
+++ tests/Feature/BreadcrumbsTest.php
@@ -0,0 +1,45 @@
+<?php declare(strict_types=1);
+
+/**
+ * Copyright (C) BaseCode Oy - All Rights Reserved
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+namespace Tests\Unit;
+
+use BaseCodeOy\Breadcrumbs\Facades\Breadcrumbs;
+use BaseCodeOy\Breadcrumbs\Generator;
+use Illuminate\Support\Facades\Route;
+use function Pest\Laravel\get;
+
+beforeEach(function (): void {
+    Route::view('home', 'home')->name('home');
+    Route::view('blog', 'blog')->name('blog');
+    Route::get(
+        'category/{category}',
+        fn () => collect(Breadcrumbs::generate())->map->toArray()->toArray(),
+    )->name('category');
+
+    Breadcrumbs::for('home', function (Generator $trail): void {
+        $trail->push('Home', route('home'));
+    });
+
+    Breadcrumbs::for('blog', function (Generator $trail): void {
+        $trail->parent('home');
+        $trail->push('Blog', route('blog'));
+    });
+
+    Breadcrumbs::for('category', function (Generator $trail, $category): void {
+        $trail->parent('blog');
+        $trail->push('Category', route('category', $category));
+    });
+});
+
+it('should generate breadcrumbs for the current route', function (): void {
+    get('category/123')
+        ->assertSee('Home')
+        ->assertSee('Blog')
+        ->assertSee('Category');
+});
